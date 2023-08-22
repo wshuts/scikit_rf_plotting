a0fbd47825f397dbf3c954cfde6ce369f20d7d27
wshuts <shutswilliam@gmail.com>
2023-08-22 15:31:39 -0500

    example with skrf.plotting.plot_smith

--- plotter.py
+++ plotter.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import skrf
-from skrf import plotting
+from skrf import plotting, Network
 
 
 def setup_plot():
@@ -13,6 +13,11 @@ def save_all_figs():
     plotting.save_all_figs()
 
 
+def create_network_from_data():
+    ring_slot = Network('data/ring slot.s2p')
+    return ring_slot
+
+
 class Plotter:
 
     @staticmethod
@@ -21,5 +26,17 @@ class Plotter:
         ax.set_title('smith')
         plotting.smith(smithR=1, chart_type='z', draw_labels=False, border=False, ax=ax, ref_imm=1.0,
                        draw_vswr=None)
+        save_all_figs()
+        return
+
+    @staticmethod
+    def plot_smith():
+        ax = setup_plot()
+        ax.set_title('plot_smith')
+        ring_slot = create_network_from_data()
+        s11 = ring_slot.s[:, 0, 0]
+        skrf.plotting.plot_smith(s11, smith_r=1, chart_type='z', x_label='Real', y_label='Imaginary',
+                                 title='Complex Plane', show_legend=True, axis='equal', ax=None, force_chart=False,
+                                 draw_vswr=None, draw_labels=False)
         save_all_figs()
         return
