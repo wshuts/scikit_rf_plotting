86393f4b84d098570224f839dd8ca6c4294d3cd0
wshuts <shutswilliam@gmail.com>
2023-08-22 14:55:40 -0500

    prep to add more plotting api calls

--- plotter.py
+++ plotter.py
@@ -3,13 +3,23 @@ import skrf
 from skrf import plotting
 
 
+def setup_plot():
+    fig, ax = plt.subplots()
+    skrf.stylely()
+    return ax
+
+
+def save_all_figs():
+    plotting.save_all_figs()
+
+
 class Plotter:
+
     @staticmethod
     def smith():
-        fig, ax = plt.subplots()
-        skrf.stylely()
-        ax.set_title('namedPlot')
-        plotting.smith(smithR=1, chart_type='z', draw_labels=False, border=False, ax=None, ref_imm=1.0,
+        ax = setup_plot()
+        ax.set_title('smith')
+        plotting.smith(smithR=1, chart_type='z', draw_labels=False, border=False, ax=ax, ref_imm=1.0,
                        draw_vswr=None)
-        plotting.save_all_figs()
+        save_all_figs()
         return
